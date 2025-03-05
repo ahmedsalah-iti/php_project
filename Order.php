21f36063ef09dd97f2a554d065c925cc93ad0861
ahmedsalah-iti <ahmedsalah.iti@gmail.com>
2025-03-05 23:45:14 +0200

    adding some comments

--- Order.php
+++ Order.php
@@ -1,13 +1,27 @@
 <?php
+/*
+mysql> describe `Order`;
++---------+----------------------------------------+------+-----+-------------------+-------------------+
+| Field   | Type                                   | Null | Key | Default           | Extra             |
++---------+----------------------------------------+------+-----+-------------------+-------------------+
+| id      | int                                    | NO   | PRI | NULL              | auto_increment    |
+| status  | enum('pending','completed','canceled') | NO   |     | NULL              |                   |
+| note    | varchar(255)                           | YES  |     | NULL              |                   |
+| user_id | int                                    | YES  | MUL | NULL              |                   |
+| room_id | int                                    | YES  | MUL | NULL              |                   |
+| date    | datetime                               | YES  |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED |
++---------+----------------------------------------+------+-----+-------------------+-------------------+
+6 rows in set (0.00 sec)
+*/
     class Order{
         private $id;
-        private $statis;
+        private $status;
         private $note;
         private $user_id;
         private $room_id;
         private $date;
 
-        public function __construct($user_id,$room_id,$note= null ,$statis = 'pending'){
+        public function __construct($user_id,$room_id,$note= null ,$status = 'pending'){
             
         }
     };
